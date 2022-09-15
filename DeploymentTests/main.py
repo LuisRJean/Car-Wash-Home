0d32d7a757d2423a243c3e4230d313ae1f67c60f
Luis Jean <ljmontreal1@gmail.com>
2022-09-15 17:33:48 -0400

    Update main.py

--- DeploymentTests/main.py
+++ DeploymentTests/main.py
@@ -14,5 +14,16 @@ def get_config():
         config = json.load(f)
     return config
 
+def get_logger():
+    """Get logger"""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
 from datetime import datetime
 from datetime import timedelta
