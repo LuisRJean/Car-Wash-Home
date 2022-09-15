fbf649fa9fa26d8c75d7dc03c032f608d635b53a
Luis Jean <ljmontreal1@gmail.com>
2022-09-15 17:33:33 -0400

    Update main.py

--- DeploymentTests/main.py
+++ DeploymentTests/main.py
@@ -7,3 +7,12 @@ import logging
 import argparse
 import subprocess
 import threading
+
+def get_config():
+    """Get config from config.json"""
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return config
+
+from datetime import datetime
+from datetime import timedelta
